b322887ad63de9606dd9649e222ed1e7dc0112d3
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-04-10 14:29:54 +0530

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+     ignores:["node-modules","dist","eslint.config.mjs"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules:{
+         "no-console":"off",
+         '@typescript-eslint/no-unsafe-call': 'off',
+         '@typescript-eslint/no-unsafe-member-access': 'off'
+    }
+  },
+);
