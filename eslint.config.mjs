e8cbf1c99a6995415b35aadf0a88ed1325130c9f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-04-11 10:29:32 +0530

    modify eslint config to ignore test files for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,13 @@ export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ['node-modules', 'dist', 'eslint.config.mjs','jest.config.js','tests/*'],
+        ignores: [
+            'node-modules',
+            'dist',
+            'eslint.config.mjs',
+            'jest.config.js',
+            'tests/*',
+        ],
     },
     {
         languageOptions: {
