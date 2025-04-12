ba675e60af66ad96ff644c87de602413567fd20f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-04-12 12:00:36 +0530

    setup typeOrm

---
+++ src/config/data-source.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from '../entity/User';
+import { Config } from '.';
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: Config.DB_HOST,
+    port: Number(Config.DB_PORT),
+    username: Config.DB_USERNAME,
+    password: Config.DB_PASSWORD,
+    database: Config.DB_NAME,
+    // do not let this be true in prod , else it'll override all stored data ( only to be `true` in dev/test)
+    synchronize: Config.NODE_ENV === 'test' || Config.NODE_ENV === 'dev',
+    logging: false,
+    entities: [User], // register every newly created entity here
+    migrations: [],
+    subscribers: [],
+});
