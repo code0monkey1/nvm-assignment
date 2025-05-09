53659348e34df91f327fbd6ee5afbb1e7971bb27
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-05-09 18:23:58 +0530

    compile typescript to javascript

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Config } from '.';
+import path from 'path';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
     // do not let this be true in prod , else it'll override all stored data ( only to be `true` in dev/test)
     synchronize: false,
     logging: false,
-    entities: ['src/entity/*.ts'], // register every newly created entity here
-    migrations: ['src/migration/*.ts'], // all .ts files in migrations folder will be considered as migration scripts
+    entities: [path.join(__dirname, '../entity/*.{js,ts}')], // register every newly created entity here
+    migrations: [path.join(__dirname, '../migration/*.{js,ts}')], // all migration files will be considered as migration scripts
     subscribers: [],
 });
