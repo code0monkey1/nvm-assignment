5f02a282e0e0403aea7a3a6e76df572ad134a2a0
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-05-10 12:50:43 +0530

    format changes to config files

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,12 @@ module.exports = {
         '^.+\.tsx?$': ['ts-jest', {}],
     },
     verbose: true,
-    collectCoverage:true,
-    coverageProvider:'v8',
-    collectCoverageFrom:[
-        'src/**/*.ts',  // include all .ts files in src
-        '!tests',       // exclude the coverage of all test files
-        '!src/migration/**', // exclude the migration files 
-        '!**/node_modules/**'  // exclude checking the code from node modules
-    ]
+    collectCoverage: true,
+    coverageProvider: 'v8',
+    collectCoverageFrom: [
+        'src/**/*.ts', // include all .ts files in src
+        '!tests', // exclude the coverage of all test files
+        '!src/migration/**', // exclude the migration files
+        '!**/node_modules/**', // exclude checking the code from node modules
+    ],
 };
